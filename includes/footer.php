ac9deb908ae2246e868de5d1f9b4ac5de4296d14
Jeremy Ward <exochos@gmail.com>
2021-05-06 22:00:19 -0700

    Create footer.php

---
+++ includes/footer.php
@@ -0,0 +1,15 @@
+<?php
+
+
+?>
+
+<!-- Footer with php -->
+<footer class="footer">
+  <div class="container">
+      <div class="text-sm-right">
+          <small>&copy;2020 - <?=date('Y')?> by <a href="contact.php" target="_blank">Jeremy Ward</a>, All Rights Reserved <a href="http://validator.w3.org/check/referer" target="_blank" rel="nofollow" title="Validate as HTML5">HTML</a> ~ <a href="http://jigsaw.w3.org/css-validator/check?uri=referer" target="_blank">CSS </a></small>
+      </div>
+</div>
+    </footer>
+  </body>
+</html>
