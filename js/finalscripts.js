c73dd01a56bb2324c8327f3bad6e92951b48717d
josh671 <jmb671@hotmail.com>
2021-06-15 00:57:24 -0700

    removed screen size function

--- js/finalscripts.js
+++ js/finalscripts.js
@@ -157,25 +157,7 @@
       }
     } 
 
-    //function to get browser widths 
-    function getWidth() {
-        if (self.innerWidth) {
-            console.log(self.innerWidth);
-            return self.innerWidth;
-          
-        }
-      
-        if (document.documentElement && document.documentElement.clientWidth) {
-            console.log(document.documentElement.clientWidth)
-          return document.documentElement.clientWidth;
-        }
-      
-        if (document.body) {
-            console.log(document.body.clientWidth);
-          return document.body.clientWidth;
-        }
-      }
-getWidth(); 
+    
   
   
   var products = [
